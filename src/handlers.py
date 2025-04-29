5091277a47c6c83910bd7044e6ccee81b87019e2
massdriver <oleg@massdriver95.ru>
2025-04-29 10:55:16 +0300

    saving

--- src/handlers.py
+++ src/handlers.py
@@ -18,24 +18,24 @@ async def handle_start(message: Message, state: FSMContext):
 async def handle_help(message: Message, state: FSMContext):
     await message.answer("/start - запустить бот\n/help - показать справку\n/book - забронировать встречу")
 
+@router.message(Command("book"))
+async def handle_book(message: Message, state: FSMContext):
+    await state.set_state(BookingSteps.CHOOSING_LOCATION)
+    keyboard = [[KeyboardButtonText(text=loc)] for loc in LOCATIONS]
+    markup = ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True)
+    await message.answer("Выберите подходящую вам локацию:", reply_markup=markup)
+
 @router.message(F.text.in_(LOCATIONS))
 async def choose_location(message: Message, state: FSMContext):
-    await state.set_state(BookingSteps.CHOOSE_LOCATION)
-    keyboard = [[KeyboardButton(loc)] for loc in LOCATIONS]
-    markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
-    await message.answer("Выберите подходящую вам локацию:", reply_markup=markup)
+    await state.set_state(BookingSteps.SELECTING_SLOT)
+    selected_location = message.text
+    await state.update_data(location=selected_location)
+    keyboard = [[KeyboardButtonText(text=slot)] for slot in SLOTS]
+    markup = ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True, one_time_keyboard=True)
+    await message.answer("Выберите подходящий вам временной слот:", reply_markup=markup)
 
 @router.message(F.text.in_(SLOTS))
 async def book_slot(message: Message, state: FSMContext):
-    await state.set_state(BookingSteps.SELECT_SLOT)
-    selected_location = message.text
-    await state.update_data(location=selected_location)
-    keyboard = [[KeyboardButton(slot)] for slot in SLOTS]
-    markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
-    await message.answer("Выберите подходящий вам временной слот:", reply_markup=markup)
-
-@router.message(StateFilter(BookingSteps.CONFIRMATION))
-async def confirm_booking(message: Message, state: FSMContext):
     await state.set_state(BookingSteps.CONFIRMATION)
     selected_timeslot = message.text
     data = await state.get_data()
@@ -54,3 +54,4 @@ async def cancel(message: Message, state: FSMContext):
 
 def register_handlers(dp: Dispatcher):
     dp.include_router(router)
+    dp.register_states(BookingSteps)  # Регистрация состояний
--- src/modules.py
+++ src/modules.py
@@ -4,8 +4,7 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters.state import StateFilter
-
-from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import Message, KeyboardButtonText, ReplyKeyboardMarkup
 from dotenv import load_dotenv
 import os
 import logging
